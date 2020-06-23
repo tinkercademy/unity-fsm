66e2f1024ddd1f358a2954cd594b86e33071780b
CrazyCyfro <crazycyfro@gmail.com>
2020-06-23 23:31:06 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+       UpdatePlayerPosition();
+    }
+
+    void UpdatePlayerPosition()
+    {
+         Vector3 motion = transform.right * MoveInputX() + transform.forward * MoveInputY();
+
+        if (motion.sqrMagnitude > 1) motion.Normalize();
+        
+        transform.position += motion * speed * Time.deltaTime;
+
+    }
+
+    float MoveInputX()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    float MoveInputY()
+    { 
+        return Input.GetAxis("Vertical");
+    }
+}
